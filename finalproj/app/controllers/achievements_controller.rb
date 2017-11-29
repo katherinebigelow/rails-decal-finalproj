4979e44ab7e251cc8bcedb0e134220dc3299b53a
Katherine Bigelow <kbigelow@berkeley.edu>
2017-11-28 21:05:01 -0800

    added achievements

--- finalproj/app/controllers/achievements_controller.rb
+++ finalproj/app/controllers/achievements_controller.rb
@@ -1,2 +1,28 @@
 class AchievementsController < ApplicationController
+
+  def fivePosts
+    @achievement = Achievement.New
+    @achievement.user_id = params[user_id]
+    @achievement.title = "Five!!!!!"
+    @achievement.description = "Made five posts."
+    @achievement.save
+	end
+
+  def tenPosts
+    @achievement = Achievement.New
+    @achievement.user_id = params[user_id]
+    @achievement.title = "Five!!!!!"
+    @achievement.description = "Made five posts."
+    @achievement.save
+	end
+
+  def fiveMinutes
+    @achievement = Achievement.New
+    @achievement.user_id = params[user_id]
+    @achievement.title = "Five!!!!!"
+    @achievement.description = "Made five posts."
+    @achievement.save
+	end
+
+
 end
--- finalproj/db/schema.rb
+++ finalproj/db/schema.rb
@@ -10,12 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123061033) do
+ActiveRecord::Schema.define(version: 20171129013115) do
 
   create_table "achievements", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_achievements_on_user_id"
   end
 
   create_table "boards", force: :cascade do |t|
